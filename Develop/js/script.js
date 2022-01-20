da56132eed4aad26e70c3ca831298dded4b9d287
REX <cssmith431@yahoo.com>
2022-01-20 16:28:27 -0500

    for help with challenge

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,8 +1,20 @@
 var date = moment().format('dddd , MMMM do YYYY, h:mm a');
 $("#currentDay").html(date);
 
+var event = {}
 
 
+var saveEvent = function() {
+    localStorage.setItem("event", JSON.stringify(tasks));
+
+
+    $(".saveBtn").on("click", function(){
+   
+        
+    })
+  };
+
+saveEvent(event);
 
 
 // var event = {};
