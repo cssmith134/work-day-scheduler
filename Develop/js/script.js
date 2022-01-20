9876938c897ff95a760391e833a091b89cd36cf2
REX <cssmith431@yahoo.com>
2022-01-20 15:39:03 -0500

    added date and time-blocks

---
+++ Develop/js/script.js
@@ -0,0 +1,13 @@
+var date = moment().format('dddd , MMMM do YYYY, h:mm a');
+$("#currentDay").html(date);
+
+
+
+
+
+// var event = {};
+
+// var saveEvent = function() {
+  //  localStorage.setItem("event", JSON.stringify(event));
+//}
+
